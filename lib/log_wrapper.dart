4ff8816a20192069e25f516f1c8a3779aba0858f
林创湖 <linchuanghu@addcn.com>
2023-07-31 13:42:20 +0800

    export文件

--- lib/log_wrapper.dart
+++ lib/log_wrapper.dart
@@ -1,7 +1,4 @@
 library log_wrapper;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'log/log.dart';
+export 'log/log_data.dart';
